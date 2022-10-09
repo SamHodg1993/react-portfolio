b908204b7a3baf08f631992333a035302a339b24
SamHodg1993 <74268707+SamHodg1993@users.noreply.github.com>
2022-10-09 10:59:43 +0100

    initial commit with header and nav complete with styling - need to create logo

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function App() {
+  return (
+    <header className="header-container">
+      <h1 className='sam-h'>Sam Hodgkinson</h1>
+      <p className='sam-h-tech-stack'>React | Typescript | TwilwindCSS | Redux | Veteran</p>
+    </header>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='nav-container'>
+        <div className='tobe-logo'>logo here</div>
+        <ul className='nav-list'>
+            <li className='nav-item'>Home</li>
+            <li className='nav-item'>About</li>
+            <li className='nav-item'>Portfolio</li>
+            <li className='nav-item'>Contact</li>
+        </ul>
+    </nav>
+  )
+}
