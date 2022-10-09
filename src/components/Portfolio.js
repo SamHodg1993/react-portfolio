f7f072a8c58906650b86a6d937d321aa841d53d1
SamHodg1993 <74268707+SamHodg1993@users.noreply.github.com>
2022-10-09 21:00:09 +0100

    Added a card and link to the code for the website in the portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SeanTrussHighlands from '../images/SeanTrussHighlands.jpg'
+import PortfolioScreenshot from '../images/PortfolioScreenshot.png'
 
 function Portfolio() {
   return (
@@ -12,10 +13,15 @@ function Portfolio() {
                 { window.innerWidth > 1200 && <p className='portfolio-card-details'>React | TailwindCSS | React-Scroll</p>}
             </div>
             <div className='portfolio-card'>
+                <a href="https://github.com/SamHodg1993/react-portfolio" target="_blank" rel="noreferrer"><img src={PortfolioScreenshot} alt="Pic of portfolio" className='portfolio-screenshot' /></a>
+                { window.innerWidth > 1200 && <p className='portfolio-card-details'>The source code for this website</p>}
+                { window.innerWidth > 1200 && <p className='portfolio-card-details'>React | TailwindCSS | React-Scroll</p>}
+            </div>
+            <div className='portfolio-card'>
                 <div className='to-be-image'>To be image</div>
                 { window.innerWidth > 1200 && <p className='portfolio-card-details'>Lord of The Rings App</p>}
                 { window.innerWidth > 1200 && <p className='portfolio-card-details'>React | TailwindCSS | React-Scroll | REST API</p>}
-        </div>
+            </div>
         </div>
     </div>
     
